c81c45464fec82483e13513c8fdb8a58f3c2608c
trojanz07 <contact@trojanzogaj.me>
2024-02-28 22:37:46 +0100

    Update progressBar.js

--- src/progressBar.js
+++ src/progressBar.js
@@ -1,5 +1,14 @@
 export function progressBar(percentage) {
-    
+    progress = Math.min(100, Math.max(0, percentage));
+
+
+    const numBars = Math.floor(percentage / 5);
+
+
+    const progressBar = `[${'='.repeat(numBars)}${' '.repeat(20 - numBars)}] ${precentage}%`;
+
+   
+    console.log(progressBar);
     }
 }
 
