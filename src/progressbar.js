10a4b2f308573992d4e118cadf10128461ba9f9c
trojanz07 <contact@trojanzogaj.me>
2024-02-28 23:47:40 +0100

    Update progressbar.js

--- src/progressbar.js
+++ src/progressbar.js
@@ -1,4 +1,9 @@
 export function progressbar(percentage) {
+    if(percentage < 1 || percentage > 100) {
+        console.error('Error: Progress should be a number between 1 and 100.');
+        return;
+    }
+    
     percentage = Math.min(100, Math.max(0, percentage));
 
 
